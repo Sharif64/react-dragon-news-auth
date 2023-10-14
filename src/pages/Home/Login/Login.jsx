15ba55632f37595fa57336d78acd26c73c96ef49
sharif64 <md.islamshariful3@gmail.com>
2023-10-14 10:10:19 +0600

    added login route

---
+++ src/pages/Home/Login/Login.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../../Shared/Navbar/Navbar";
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h3>This is login page.</h3>
+        </div>
+    );
+};
+
+export default Login;
